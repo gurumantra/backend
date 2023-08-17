1eb64e18704ad3b1f703ead08ee4f60dd1659738
sundaradh <abcsundaradhikari123@gmail.com>
2023-08-17 13:45:54 +0545

    register on admin site for all model

---
+++ katha/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Katha
+
+admin.site.register(Katha)
+
---
+++ poojabidhi/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import PoojaBidhi
+
+admin.site.register(PoojaBidhi)
