2e08a604666b7f8d11d93ea14de9ee1833ba542c
sundaradh <abcsundaradhikari123@gmail.com>
2023-09-10 14:44:52 +0545

    update allowed host

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-83@=l4codd3wbm$%1sues_%d^#=1i!gfhwsfkkm34xk6!1ystv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 SITE_ID = 1
 
 # Application definition
